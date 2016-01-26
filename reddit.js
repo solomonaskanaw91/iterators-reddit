a6d474bc37dd9180ab39efd13bffc63949e4724a
user <user@users-MacBook-Pro-2.local>
2016-01-25 19:47:48 -0800

    reddit iterators homework

--- reddit.js
+++ reddit.js
@@ -1,5 +1,45 @@
+//Problem #1 ForEach by Titile
 var reddit = require("./data.json")
+var posts = reddit.data.children;
 
-// Write your solutions below
+posts.forEach(function (redChild) {
+	console.log(redChild.data.title);
+}
 
-console.log(reddit["data"]);
+//Problem #2 Map Iterators with Reddit
+
+var linkMap = posts.map(function (person) {
+	return 'http://reddit.com' + person.data.permalink
+});
+
+console.log(linkMap);
+
+//Problem # 3
+var postFilter = function(post) {
+	if (post.data.selftext!==""){
+		return true
+
+	}
+}
+
+
+
+
+
+var filteredPosts = posts.filter(postFilter)
+
+console.log(filteredPosts)
+
+//Problem # 4 
+
+ var scoreCount = post.map(function (totalScore) {
+ 	return totalScore.data.score;
+ });
+
+ var add = function (a, b){
+ 	return (a + b);
+ };
+
+
+
+
